3a938de4e82622757cc5d57b979ffaa8d282cac5
Kai Rösken <69394132+fastrockstar@users.noreply.github.com>
2023-03-29 20:26:35 +0200

    Update slack_notifier.py

--- slack_notifier.py
+++ slack_notifier.py
@@ -1,15 +1,54 @@
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
-
 class SlackNotifier:
-    def __init__(self, slack_token: str, slack_channel: str):
-        self.slack_client = WebClient(token=slack_token)
+    def __init__(self, slack_token, slack_channel):
+        """
+        Initializes a new instance of the SlackNotifier class.
+
+        :param slack_token: The Slack API token.
+        :param slack_channel: The name or ID of the Slack channel to post messages to.
+        """
+        self.slack_token = slack_token
         self.slack_channel = slack_channel
-    
-    def send_message(self, message: str):
+
+    def send_message(self, message, log_group_name=None):
+        """
+        Sends a message to the configured Slack channel.
+
+        :param message: The message to send.
+        :param log_group_name: The name of the CloudWatch Logs group containing the error log (optional).
+        """
+        # Initialize a new Slack API client with the provided token.
+        client = WebClient(token=self.slack_token)
+
+        # If a log group name is provided, generate a Log group button for the Slack message.
+        if log_group_name:
+            # Construct the URL for the Log group in the AWS Management Console.
+            log_group_url = f"https://console.aws.amazon.com/cloudwatch/home?region={self.region}#logsV2:log-groups/log-group/{log_group_name}"
+            # Construct the attachment for the Slack message, containing the error message and the Log group button.
+            log_group_button = {
+                "type": "button",
+                "text": "View logs in CloudWatch",
+                "url": log_group_url
+            }
+            attachments = [{
+                "color": "#ff0000",
+                "title": "Error Log",
+                "text": message,
+                "actions": [log_group_button]
+            }]
+            message = ""  # Clear the message body if an attachment is included.
+        else:
+            attachments = None
+
         try:
-            response = self.slack_client.chat_postMessage(channel=self.slack_channel, text=message)
-            print(f"Sent message: {message}")
+            # Use the Slack API client to post a message to the configured channel.
+            response = client.chat_postMessage(
+                channel=self.slack_channel,
+                text=message,
+                attachments=attachments
+            )
+            return response["ts"]
         except SlackApiError as e:
-            print(f"Error sending message: {e}")
+            print("Error sending message: {}".format(e))
